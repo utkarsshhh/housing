e46883dfd521d91c6b6ebb1177271be087ec0c2d
utkarsshhh <30666475+utkarsshhh@users.noreply.github.com>
2021-12-07 00:35:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 from flask import request,jsonify
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-cors = CORS(app)
+CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 ld = open('db.json')
 db = json.load(ld)
